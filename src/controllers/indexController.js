1ff249d20f09ce7c8e0bec4e3b505f66d73590bf
Fede Angeles <fedeangeles23@gmail.com>
2022-12-25 06:22:06 -0300

    detalles dinamicos y json

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,8 +1,17 @@
+let menu = require('../data/menu.json')
+
 module.exports = {
+
     home:(req,res) => {
-        return res.render('index')
+        return res.render('index',{ /* Renderiza la vista index */
+            menu    /* Envía desde el backend hacia la vista index, menu desde el controlador */
+        })
     },
+
     menu: (req,res) => {
+        return res.render ('detalleMenu')
+    },
+    detalle: (req,res) => {
         return res.render ('detalleMenu')
     }
 }
